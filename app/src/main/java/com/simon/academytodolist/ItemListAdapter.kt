b2ed88afea70a8c9a89a924c3d90afb692b396c2
Simon <szymon.stypka@o2.pl>
2023-01-04 14:09:31 +0100

    Task 5 (Every second element should display random web view instead of the original one)

--- app/src/main/java/com/simon/academytodolist/ItemListAdapter.kt
+++ app/src/main/java/com/simon/academytodolist/ItemListAdapter.kt
@@ -1,34 +1,63 @@
 package com.simon.academytodolist
 
+import android.content.ClipData.Item
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebView
 import androidx.recyclerview.widget.RecyclerView
 import com.simon.academytodolist.databinding.ItemListBinding
+import com.simon.academytodolist.databinding.ItemWebBinding
+import kotlin.random.Random
 
-class ItemListAdapter(var items: ArrayList<String>, val listener: (String, Int) -> Unit): RecyclerView.Adapter<ItemListAdapter.ViewHolder>() {
+class ItemListAdapter(var items: ArrayList<String>, val listener: (String, Int) -> Unit): RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+    val urls = arrayListOf(
+        "https://static.dw.com/image/64052597_905.jpg",
+        "https://www.google.com/",
+        "https://s3.viva.pl/newsy/grumpy-cat-najpopularniejszy-kot-instagrama-567749-GALLERY_BIG.jpg",
+        "https://twitter.com/"
+    )
 
     fun setList(list: ArrayList<String>){
         this.items = list
         notifyDataSetChanged()
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        return ViewHolder(ItemListBinding.inflate(LayoutInflater.from(parent.context), parent, false))
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return when(viewType){
+            0 -> ViewHolder(ItemListBinding.inflate(LayoutInflater.from(parent.context), parent, false))
+            else -> WebViewHolder(ItemWebBinding.inflate(LayoutInflater.from(parent.context), parent, false))
+        }
     }
 
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
         val item: String = items[position]
-        holder.bind(item)
+        when(holder){
+            is ViewHolder -> holder.bind(item)
+            is WebViewHolder -> holder.bind(urls.get(Random.nextInt(0, urls.size)))
+        }
     }
 
     override fun getItemCount() = items.size
+
+    override fun getItemViewType(position: Int): Int {
+        return if(position % 2 == 0) 0 else 1
+    }
 
     inner class ViewHolder(private val binding: ItemListBinding): RecyclerView.ViewHolder(binding.root){
 
         fun bind(item: String){
             binding.itemName.text = item
             binding.deleteBtn.setOnClickListener{ listener(item, adapterPosition) }
+        }
+    }
+
+    inner class WebViewHolder(private val binding: ItemWebBinding): RecyclerView.ViewHolder(binding.root){
+
+        fun bind(url: String){
+            val webView = binding.webView
+            webView.loadUrl(url)
         }
     }
 }
